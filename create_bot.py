81480769890ab16a59106fc4f46cf441f4bcf325
Real Name <prodoktor517@gmail.com>
2023-10-16 11:17:39 +0600

    connec db

--- create_bot.py
+++ create_bot.py
@@ -14,12 +14,13 @@ TOKEN = '6684662073:AAGw6gZtBmQ0QWpoRnvoyAsZeswyDUQipnw'
 # ip=os.getenv("ip")
 ip = 'localhost'
 # PGUSER=str(os.getenv("PGUSER"))
-PGUSER = 'postgres'
+# PGUSER = 'postgres'
 # PGPASSWORD = str(os.getenv("PGPASSWORD"))
-PGPASSWORD = 'cont2023'
+# PGPASSWORD = 'cont2023'
 # DATABASE = str(os.getenv("DATABASE"))
-DATABASE = 'vostok'
-POSTGREURI=f'postgresql://{PGUSER}:{PGPASSWORD}@{ip}/{DATABASE}'
+# DATABASE = 'vostok'
+# POSTGREURI=f'postgresql://{PGUSER}:{PGPASSWORD}@{ip}/{DATABASE}'
+POSTGREURI = 'postgres://oudkamoyueapuv:26ce0912ff0ce412bd22fdce6c0111954f42076a9af30010ad01e0fa2a8fd633@ec2-34-242-154-118.eu-west-1.compute.amazonaws.com:5432/d8sr8luiviue8r'
 operator = 6398627453
 
 bot = Bot(token=TOKEN)
