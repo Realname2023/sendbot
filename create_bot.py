e63988f16a1aa27154b911f05bd5bb8c7eab0949
Real Name <prodoktor517@gmail.com>
2023-10-17 10:18:25 +0600

    var database

--- create_bot.py
+++ create_bot.py
@@ -13,6 +13,8 @@ TOKEN = str(os.getenv("TOKEN"))
 
 URL_APP = 'https://deplbot-a5f641a62530.herokuapp.com/'
 
+POSTGREURI = str(os.getenv("DATABASE_URL"))
+
 # ip=os.getenv("ip")
 ip = 'localhost'
 # PGUSER=str(os.getenv("PGUSER"))
@@ -22,7 +24,7 @@ ip = 'localhost'
 # DATABASE = str(os.getenv("DATABASE"))
 # DATABASE = 'vostok'
 # POSTGREURI=f'postgresql://{PGUSER}:{PGPASSWORD}@{ip}/{DATABASE}'
-POSTGREURI = 'postgres://oudkamoyueapuv:26ce0912ff0ce412bd22fdce6c0111954f42076a9af30010ad01e0fa2a8fd633@ec2-34-242-154-118.eu-west-1.compute.amazonaws.com:5432/d8sr8luiviue8r'
+# POSTGREURI = 'postgres://oudkamoyueapuv:26ce0912ff0ce412bd22fdce6c0111954f42076a9af30010ad01e0fa2a8fd633@ec2-34-242-154-118.eu-west-1.compute.amazonaws.com:5432/d8sr8luiviue8r'
 operator = 6398627453
 
 bot = Bot(token=TOKEN)
